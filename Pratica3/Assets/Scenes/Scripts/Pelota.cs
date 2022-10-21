e47cd3d792d75653a93e8699ee2037c18d5a6ae3
Brandol-oh <71796242+Brandol-oh@users.noreply.github.com>
2022-10-20 23:25:32 -0700

    Practica 3 IHC

---
+++ Pratica3/Assets/Scenes/Scripts/Pelota.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pelota : Interactable
+{
+    private Rigidbody rb;
+    public Vector3 direction;
+    public float speed = 10f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    public override void Interact()
+    {
+        base.Interact();
+        rb.AddForce(direction * speed, ForceMode.Force);
+    }
+}
